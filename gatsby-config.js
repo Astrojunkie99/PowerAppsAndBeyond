b97e2a11fbe5cf6774396e3fd92ea2c3050d4bc7
Paul Robinson <paul.robinson@essentialwhsapps365.com.au>
2019-09-16 22:48:20 +1000

    adding SASS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `PowerApps and Beyond`,
+    description: `A personal blog ranging from topics including PowerApps, my adventures as an accidental web developer to discussions in space`,
+    author: `Paul Robinson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sass`,
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
