6f1b1aa72b89d95c97f20ece088e640efee4763d
Paul Robinson <paul.robinson@essentialwhsapps365.com.au>
2019-09-18 04:59:10 +1000

    Sidenav changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,9 @@
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Navbar from "../components/navbar";
 
-import Header from "./header"
-
-
-const Layout = ({ children }) => {
+const Layout = () => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -15,32 +12,16 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
-
+  `);
+  
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    
+    <div>
+     
+        <Navbar />
+    
+    </div>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from 'react'
+
+import { FaBars} from 'react-icons/fa';
+import { FaTimes} from 'react-icons/fa';
+import styles from "../styles/navbar.module.scss";
+import {Link} from "gatsby";
+import Mars from "../images/Mars.jpg";
 
 const Navbar = () => {
 
     return (
-        <div>
+        <header>
+        <input id={styles.sidebar__trigger} className={styles.sidebar__trigger} type="checkbox" />
+            <label for={styles.sidebar__trigger}>
+            <span className={styles.sidebar__button+" "+ styles.sidebar__buttonOpen}><FaBars/></span>
+            <span className={styles.sidebar__button+" "+ styles.sidebar__buttonClose}><FaTimes /></span>
+            </label><h2>PowerApps <span className={styles.logo}>and Beyond</span></h2>
             
-        </div>
+        
+
+            <ul className={styles.sidebar}>
+                <Link activeStyle={{color:"#ffebee", textDecoration:"none"}}><li>Home Page</li></Link>
+                <Link activeStyle={{color:"#ffebee", textDecoration:"none"}}><li>APOD</li></Link>
+                <Link activeStyle={{color:"#ffebee", textDecoration:"none"}}><li>Rocket Launches</li></Link>
+                <Link activeStyle={{color:"#ffebee", textDecoration:"none"}}><li>Random Hubble Pics</li></Link>
+            </ul>
+            
+            <img src={Mars} className={styles.imageDisplay} />
+        </header>
     )
 }
 
