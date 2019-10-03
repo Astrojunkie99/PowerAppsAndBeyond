097b01a607683a26e5b39b08e3f08f96056ecc9e
Paul Robinson <paul.robinson@essentialwhsapps365.com.au>
2019-10-03 23:36:48 +1000

    APOD 4

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,91 +1,89 @@
-const path = require('path')
-const _ = require('lodash')
+const path = require("path")
+const _ = require("lodash")
 const kebabCase = require("lodash.kebabcase")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
-    if (node.internal.type === 'MarkdownRemark') {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
 }
 
 module.exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
-    const tagTemplate = path.resolve('./src/components/templates/tags-page.js')
-    const blogTemplate = path.resolve('./src/components/templates/blog-post.js')
-    
-    const res = await graphql(`
-        query {
-            allMarkdownRemark (sort:{
-                fields: [frontmatter___tags]
-                order: ASC
-            })
-             {
-                edges {
-                    node {
-                        excerpt
-                        fields {
-                            slug
-                        }
-                        frontmatter {
-                            title
-                            tags 
-                        }
-                    }
-                }
+  const { createPage } = actions
+  const tagTemplate = path.resolve("./src/components/templates/tags-page.js")
+  const blogTemplate = path.resolve("./src/components/templates/blog-post.js")
+
+  const res = await graphql(`
+    query {
+      allMarkdownRemark(sort: { fields: [frontmatter___tags], order: ASC }) {
+        edges {
+          node {
+            excerpt
+            fields {
+              slug
             }
-        }
-    `)
-
-    const posts=res.data.allMarkdownRemark.edges
-    res.data.allMarkdownRemark.edges.forEach((edge) => {
-        createPage({
-            component: blogTemplate,
-            path: `/posts/${edge.node.fields.slug}`,
-            context: {
-                slug: edge.node.fields.slug
+            frontmatter {
+              title
+              tags
             }
-        })
-    })
-    let tags = []
-    _.each(posts, edge => {
-        if (_.get(edge, 'node.frontmatter.tags')){
-        tags= tags.concat(edge.node.frontmatter.tags)
+          }
         }
-    })
- let tagPostCounts = {}
- tags.forEach(tag => {
-     tagPostCounts[tag] = (tagPostCounts[tag] || 0) +1;
- })
- tags = _.uniq(tags)
+      }
+    }
+  `)
 
- createPage({
-     path: `/tags`,
-     component: tagTemplate,
-     context: {
-         tags,
-         tagPostCounts
-     },
- })
-
- const tagsTemplate = path.resolve("src/components/templates/tags.js")
-    // Make tag pages
-    tags.forEach(tag => {
+  const posts = res.data.allMarkdownRemark.edges
+  res.data.allMarkdownRemark.edges.forEach(edge => {
     createPage({
-        path: `/tags/${kebabCase(tag)}/`,
-        component: tagsTemplate,
-        context: {
-            
-            tag,
-        },
+      component: blogTemplate,
+      path: `/posts/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
     })
-    })
- }
+  })
+  let tags = []
+  _.each(posts, edge => {
+    if (_.get(edge, "node.frontmatter.tags")) {
+      tags = tags.concat(edge.node.frontmatter.tags)
+    }
+  })
+  let tagPostCounts = {}
+  tags.forEach(tag => {
+    tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+  })
+  tags = _.uniq(tags)
 
+  createPage({
+    path: `/tags`,
+    component: tagTemplate,
+    context: {
+      tags,
+      tagPostCounts,
+    },
+  })
+
+  const tagsTemplate = path.resolve("src/components/templates/tags.js")
+  // Make tag pages
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${kebabCase(tag)}/`,
+      component: tagsTemplate,
+      context: {
+        tag,
+      },
+    })
+  })
+}
